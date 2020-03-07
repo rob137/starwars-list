7b298b4533677a4f230d741d19f7250db58e5d7b
rob <robertaxelkirby@gmail.com>
2020-03-07 11:30:45 +0000

    Basic setup for urql (Provider, client, useQuery)

--- toolbox/src/components/FilmList.stories.tsx
+++ toolbox/src/components/FilmList.stories.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { createClient, Provider, useQuery } from 'urql';
+
+const client = createClient({
+  url: 'https://cors-anywhere.herokuapp.com/swapi.graph.cool',
+});
 
 export default {
   title: 'Film List',
 };
 
+const getFoo = ``;
+
 export const Default = () => {
   // this is where you should put urql Provider
-  return <FilmListComponent />;
+  return (
+    <Provider value={client}>
+      <FilmListComponent />;
+    </Provider>
+  );
 };
 
 const FilmListComponent = () => {
+  const [result] = useQuery({
+    query: getAllFilms,
+  });
+
+  if (result.fetching) return <div>'Loading...'</div>;
+  if (result.error) return <div>'Oh no!'</div>;
   // this is where you should put urql useQuery
   return <div>Edit me to see changes in the storybook!</div>;
 };
